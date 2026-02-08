ba85a1adf133b72fb2e990d81111fe1a8fc3a3bd
mustafairfan <mi6525442@gmail.com>
2026-02-09 04:58:42 +0500

    nowlast commit

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import orderRouter from "./routes/orderRoute.js";
 
 // INFO: Create express app mern stack
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 connectDB();
 connectCloudinary();
 
