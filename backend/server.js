3b97b3af0a5e84e115e01a4eee6513483d4376f0
mustafairfan <mi6525442@gmail.com>
2026-02-09 05:07:42 +0500

    new commit

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,45 @@
+// import express from "express";
+// import cors from "cors";
+// import "dotenv/config";
+// import connectDB from "./config/mongodb.js";
+// import connectCloudinary from "./config/cloudinary.js";
+// import userRouter from "./routes/userRoute.js";
+// import productRouter from "./routes/productRoute.js";
+// import cartRouter from "./routes/cartRoute.js";
+// import orderRouter from "./routes/orderRoute.js";
+
+// // INFO: Create express app mern stack
+// const app = express();
+// const port = process.env.PORT;
+// connectDB();
+// connectCloudinary();
+
+// // INFO: Middleware
+// app.use(express.json());
+// app.use(cors({
+//   origin: "https://my-ecommerce-admin-ruby.vercel.app", 
+//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//   allowedHeaders: ["Content-Type", "token"], 
+//   credentials: true
+// }));
+
+
+// // INFO: API endpoints
+// app.use("/api/user", userRouter);
+// app.use("/api/product", productRouter);
+// app.use('/api/cart',cartRouter)
+// app.use('/api/order',orderRouter)
+// // INFO: Default route
+// app.get("/", (req, res) => {
+//   res.send("API is running...");
+// });
+
+// // INFO: Start server
+// app.listen(port, () =>
+//   console.log(`Server is running on at http://localhost:${port}`)
+// );
+
+
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
@@ -8,33 +50,40 @@ import productRouter from "./routes/productRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
 
-// INFO: Create express app mern stack
+
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 connectDB();
 connectCloudinary();
 
-// INFO: Middleware
+const allowedOrigins = [
+  "https://my-ecommerce-front-end.vercel.app", // User Website
+  "https://my-ecommerce-admin-ruby.vercel.app", // Admin Panel
+];
+
 app.use(express.json());
 app.use(cors({
-  origin: "https://my-ecommerce-admin-ruby.vercel.app", 
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS policy block: This origin is not allowed"));
+    }
+  },
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "token"], 
   credentials: true
 }));
 
-
-// INFO: API endpoints
 app.use("/api/user", userRouter);
 app.use("/api/product", productRouter);
-app.use('/api/cart',cartRouter)
-app.use('/api/order',orderRouter)
-// INFO: Default route
+app.use('/api/cart', cartRouter);
+app.use('/api/order', orderRouter);
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-// INFO: Start server
 app.listen(port, () =>
-  console.log(`Server is running on at http://localhost:${port}`)
+  console.log(`Server is running at port: ${port}`)
 );
