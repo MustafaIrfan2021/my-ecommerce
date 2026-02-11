5ce4a29fa3c7bf22dc22c15bc8470ee545d98625
mustafairfan <mi6525442@gmail.com>
2026-02-11 06:21:51 +0500

    new commit

--- frontend/src/components/OurPolicy.jsx
+++ frontend/src/components/OurPolicy.jsx
@@ -5,7 +5,7 @@ const OurPolicy = () => {
   return (
     <div className='flex flex-col justify-around gap-12 py-8 text-xs text-center text-gray-700 sm:flex-row sm:gap-2 sm:text-sm md:text-base'>
         <div>
-  <img src={assets.exchange_icon} className='w-12 m-auto mb-3' alt="Terms" />
+  <img src={assets.conditions} className='w-12 m-auto mb-3' alt="Terms" />
   <p className='mb-2 font-semibold'>Terms & Conditions</p>
   <p className='text-gray-400'>
     By placing an order on our website, you agree to our pricing, payment processing,
