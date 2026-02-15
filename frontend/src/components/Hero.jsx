eb5eb1989f1db5aa05484810a816f8e96fee197f
mustafairfan <mi6525442@gmail.com>
2026-02-16 03:51:25 +0500

    new commit

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,38 +1,38 @@
-import React from 'react'
-import { assets } from '../assets/assets'
+// import React from 'react'
+// import { assets } from '../assets/assets'
 
-const Hero = () => {
-  return (
-    // min-h-[500px] se minimum height set hogi aur h-[70vh] se screen ka 70% area cover hoga
-    <div className='flex flex-col border border-gray-400 sm:flex-row min-h-[500px] md:h-[70vh] overflow-hidden'>
+// const Hero = () => {
+//   return (
+//     // min-h-[500px] se minimum height set hogi aur h-[70vh] se screen ka 70% area cover hoga
+//     <div className='flex flex-col border border-gray-400 sm:flex-row min-h-[500px] md:h-[70vh] overflow-hidden'>
         
-        {/* Hero left side */}
-        <div className='flex items-center justify-center w-full py-20 sm:w-1/2 sm:py-0'>
-            <div className='text-[#414141]'>
-                <div className='flex items-center gap-2'>
-                    <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
-                    <p className='text-sm font-medium md:text-base'>OUR BEST SELLERS</p>
-                </div>
-                <h1 className='text-3xl leading-relaxed sm:py-3 lg:text-5xl prata-regular'>Latest Arrivals</h1>
-                <div className='flex items-center gap-2'>
-                    <p className='text-sm font-semibold md:text-base'>SHOP NOW</p>
-                    <p className='w-8 md:w-11 h-[1px] bg-[#414141]'></p>
-                </div>
-            </div>
-        </div>
+//         {/* Hero left side */}
+//         <div className='flex items-center justify-center w-full py-20 sm:w-1/2 sm:py-0'>
+//             <div className='text-[#414141]'>
+//                 <div className='flex items-center gap-2'>
+//                     <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
+//                     <p className='text-sm font-medium md:text-base'>OUR BEST SELLERS</p>
+//                 </div>
+//                 <h1 className='text-3xl leading-relaxed sm:py-3 lg:text-5xl prata-regular'>Latest Arrivals</h1>
+//                 <div className='flex items-center gap-2'>
+//                     <p className='text-sm font-semibold md:text-base'>SHOP NOW</p>
+//                     <p className='w-8 md:w-11 h-[1px] bg-[#414141]'></p>
+//                 </div>
+//             </div>
+//         </div>
 
-        {/* Hero right side */}
-        {/* object-cover se image stretch nahi hogi, balki box ko fill karegi */}
-        <img 
-          className='w-full sm:w-1/2 object-cover' 
-          src="https://images.unsplash.com/photo-1551028719-00167b16eac5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGVhdGhlciUyMGphY2tldHxlbnwwfHwwfHx8MA%3D%3D" 
-          alt="Hero Image" 
-        />
-    </div>
-  )
-}
+//         {/* Hero right side */}
+//         {/* object-cover se image stretch nahi hogi, balki box ko fill karegi */}
+//         <img 
+//           className='w-full sm:w-1/2 object-cover' 
+//           src="https://images.unsplash.com/photo-1551028719-00167b16eac5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGVhdGhlciUyMGphY2tldHxlbnwwfHwwfHx8MA%3D%3D" 
+//           alt="Hero Image" 
+//         />
+//     </div>
+//   )
+// }
 
-export default Hero
+// export default Hero
 
 
 // import React from 'react'
@@ -66,3 +66,35 @@ export default Hero
 // }
 
 // export default Hero
+
+
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Hero = () => {
+  return (
+    /* overflow-hidden lagaya hai taake bahar na nikle aur border ko rounded kiya hai */
+    <div className='flex flex-col sm:flex-row border border-gray-400 overflow-hidden rounded-sm'>
+        
+        {/* Hero left side */}
+        <div className='w-full sm:w-1/2 h-[300px] sm:h-auto overflow-hidden'>
+            <img 
+              className='w-full h-full object-cover hover:scale-105 transition-all duration-500' 
+              src="https://media.istockphoto.com/id/92066451/photo/black-leather-motorcycle-jacket.webp?a=1&b=1&s=612x612&w=0&k=20&c=aDLL1FU_F4ZWU_qpGFvjcabfi9oUqf1rAgr2J0rVdoI=" 
+              alt="Premium Leather Jacket" 
+            />
+        </div>
+
+        {/* Hero right side */}
+        <div className='w-full sm:w-1/2 h-[300px] sm:h-auto overflow-hidden border-t sm:border-t-0 sm:border-l border-gray-400'>
+            <img 
+              className='w-full h-full object-cover hover:scale-105 transition-all duration-500' 
+              src='https://images.unsplash.com/photo-1489286696299-aa7486820bd5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bGVhdGhlciUyMGphY2tldHN8ZW58MHx8MHx8fDA%3D' 
+              alt="Fashion Style" 
+            />
+        </div>
+    </div>
+  )
+}
+
+export default Hero
